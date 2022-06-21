a37f305d0f25d9e88b1b31e80882aaf3902962c6
tobiaspsp <tobias@powershell.one>
2022-06-21 11:41:32 +0200

    Initial Commit

---
+++ 3_ManagingLargeFiles.ps1
@@ -0,0 +1,52 @@
+﻿# Get-Content can only deal with relatively small files
+# for huge files, we need a command that can read anywhere in a large file
+
+# here is a flavor of it
+
+function Get-ContentEx
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [string]
+        $Path,
+
+        [Parameter(Mandatory)]
+        [int64]
+        $Position,
+
+        [System.Text.Encoding]
+        $Encoding = [System.Text.Encoding]::ASCII,
+
+        [int]
+        $BufferSize = 128,
+
+        [switch]
+        $DetectBOM
+    )
+
+    $reader = [System.IO.StreamReader]::new($Path, $Encoding, $DetectBOM.IsPresent, $BufferSize)
+    [System.IO.Stream] $baseStream = $reader.BaseStream
+    
+    $null = $baseStream.Seek($Position, [System.IO.SeekOrigin]::Begin)
+    $reader.DiscardBufferedData()
+    # find the start of the current text line
+    # if we don't hit a line end (ASCII 10) or the beginning (0)...
+    while ($baseStream.Position -ne 0L -band $baseStream.ReadByte() -ne 10)
+    {
+        # ...we go back two chars (the one just read plus one more):
+        $null = $baseStream.Seek(-2L, [System.IO.SeekOrigin]::Current)
+    }
+
+    # now we are at a line start for sure:
+    [int64] $positionNew = $baseStream.Position
+    
+    # read line and return:
+    [PSCustomObject]@{
+        Text = [string]$reader.ReadLine()
+        Position = $positionNew
+    }
+    
+    # dispose reader and free memory:
+    $reader.Dispose()
+}
